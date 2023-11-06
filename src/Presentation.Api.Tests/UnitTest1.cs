0946066fc075d7aeff89b0f09a6338faf740ba73
Gilberto Magalhães <gbmal@vestas.com>
2023-11-06 10:36:05 +0000

    Add api and workflow

---
+++ src/Presentation.Api.Tests/UnitTest1.cs
@@ -0,0 +1,7 @@
+namespace Presentation.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test() => Assert.True(1 == 1);
+}
---
+++ src/Presentation.Api/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(
+        policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+
+builder.Host.UseSystemd();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+var people = new List<string>
+{
+    "Ali",
+    "Beatriz",
+    "Charles",
+    "Diya",
+    "Eric",
+    "Fatima",
+    "Gabriel",
+    "Hanna"
+};
+
+app.MapGet(
+    "/api/people",
+    Results<Ok<List<string>>, NotFound> (IConfiguration configuration)
+        => TypedResults.Ok(people.OrderBy(x => Random.Shared.Next()).ToList()));
+
+app.UseHttpsRedirection().UseCors();
+app.Run();
